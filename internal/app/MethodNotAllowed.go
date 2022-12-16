92c203edd24eea3c3435ce106f2b6ee679489297
kerelape <kerelape@gmail.com>
2022-12-16 13:40:35 +0500

    Add MethodNotAllowed

---
+++ internal/app/MethodNotAllowed.go
@@ -0,0 +1,11 @@
+package app
+
+import "net/http"
+
+func MethodNotAllowed(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
+}
+
+func MethodNotAllowedHandler() http.Handler {
+	return http.HandlerFunc(MethodNotAllowed)
+}
