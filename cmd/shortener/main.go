0385fb279eef17de212aea4ea1a87bfb08916e6f
kerelape <kerelape@gmail.com>
2022-12-16 17:16:36 +0500

    Compose app

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,36 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/kerelape/urlshortener/internal/app"
+)
+
+const UrlShortenerPath = "/"
+
+func main() {
+	var shortener = app.NewDatabaseShortener(
+		app.NewFakeDatabase(),
+		app.NewJoinedAlphabet(
+			app.NewAsciiAlphabet(48, 57),
+			app.NewJoinedAlphabet(
+				app.NewAsciiAlphabet(65, 90),
+				app.NewAsciiAlphabet(97, 122),
+			),
+		),
+	)
+	var shortenerHttpInterface = app.NewMethodFilter(
+		http.MethodPost,
+		app.NewShortenHandler(shortener),
+		app.NewMethodFilter(
+			http.MethodGet,
+			app.NewRevealHandler(UrlShortenerPath, shortener),
+			app.MethodNotAllowedHandler(),
+		),
+	)
+
+	var service = http.NewServeMux()
+	service.Handle(UrlShortenerPath, shortenerHttpInterface)
+	log.Fatal(http.ListenAndServe(":8080", service))
+}
