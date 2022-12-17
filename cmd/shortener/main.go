09956a4aedc494d338a2a3fa92ae18f013324948
kerelape <kerelape@gmail.com>
2022-12-17 15:10:25 +0500

    dbg

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -12,10 +12,25 @@ const URLShortenerPath = "/"
 func main() {
 	http.Handle(
 		URLShortenerPath,
-		app.NewShortenerHTTPInterface(
-			app.NewDatabaseShortener(app.NewFakeDatabase()),
-			URLShortenerPath,
-		),
+		&Middleware{
+			Origin: app.NewShortenerHTTPInterface(
+				app.NewDatabaseShortener(app.NewFakeDatabase()),
+				URLShortenerPath,
+			),
+			Middleware: func(w http.ResponseWriter, r *http.Request) {
+				println(r.Method, r.Host, r.Proto, r.RequestURI, r.URL)
+			},
+		},
 	)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+type Middleware struct {
+	Origin     http.Handler
+	Middleware http.HandlerFunc
+}
+
+func (middleware *Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	middleware.Middleware(w, r)
+	middleware.Origin.ServeHTTP(w, r)
 }
