039e38defe179bab6e7405795679c38aacbadce0
kerelape <kerelape@gmail.com>
2022-12-18 14:54:27 +0500

    Change status

--- internal/app/ui/http/reveal_handler.go
+++ internal/app/ui/http/reveal_handler.go
@@ -22,7 +22,7 @@ func NewRevealHandler(shortener model.Shortener, parser RevealRequestParser) *Re
 func (handler *RevealHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var origin, err = handler.Shortener.Reveal(handler.Parser.ParseShortURL(r))
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
 	w.Header().Add("Location", origin)
