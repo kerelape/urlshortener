457134f029c12e2aea7658146520f0cd9a475b83
kerelape <kerelape@gmail.com>
2023-07-26 01:39:44 +0500

    Add printBuildInfo function

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -27,20 +27,9 @@ var (
 )
 
 func main() {
-	if buildVersion == "" {
-		buildVersion = "N/A"
-	}
-	fmt.Printf("Build version: %s\n", buildVersion)
-
-	if buildDate == "" {
-		buildDate = "N/A"
-	}
-	fmt.Printf("Build date: %s\n", buildDate)
-
-	if buildCommit == "" {
-		buildCommit = "N/A"
-	}
-	fmt.Printf("Build commit: %s\n", buildCommit)
+	printBuildInfo("Build version", buildVersion)
+	printBuildInfo("Build date", buildDate)
+	printBuildInfo("Build commit", buildCommit)
 
 	config, configError := app.InitConfig()
 	if configError != nil {
@@ -124,4 +113,11 @@ func initService(
 	router.Mount("/", webUI.Route())
 	router.Mount("/debug/pprof", http.HandlerFunc(pprof.Index))
 	return router
+}
+
+func printBuildInfo(label, value string) {
+	if len(value) == 0 {
+		value = "N/A"
+	}
+	fmt.Printf("%s: %s\n", label, value)
 }
